f8dc0df5fd7468353092ded230371e0f182b8462
quangptt0910 <quangptt0910@gmail.com>
2024-09-09 22:51:57 +0200

    Update

--- src/main/java/com/qttp/demo_shops/controller/ImageController.java
+++ src/main/java/com/qttp/demo_shops/controller/ImageController.java
@@ -1,20 +1,27 @@
 package com.qttp.demo_shops.controller;
 
 import com.qttp.demo_shops.dto.ImageDto;
+import com.qttp.demo_shops.exceptions.ResourceNotFoundException;
 import com.qttp.demo_shops.model.Image;
 import com.qttp.demo_shops.model.Product;
 import com.qttp.demo_shops.response.ApiResponse;
 import com.qttp.demo_shops.service.image.IImageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.sql.SQLException;
 import java.util.List;
+
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RequiredArgsConstructor
 @RestController
@@ -31,5 +38,55 @@ public class ImageController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Upload failed!", e.getMessage()));
 
         }
+
     }
+
+    @GetMapping("/image/download/{imageId}")
+    public ResponseEntity<Resource> downloadImage(@PathVariable Long imageId) throws SQLException {
+        Image image = imageService.getImageById(imageId);
+        ByteArrayResource resource = new ByteArrayResource(image.getImage().getBytes(1, (int) image.getImage().length()));
+        return  ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" +image.getFileName() + "\"")
+                .body(resource);
+    }
+
+    @PutMapping("/image/{imageId}/update")
+    public ResponseEntity<ApiResponse> updateImage(@PathVariable Long imageId, @RequestBody MultipartFile file) {
+        try {
+            Image image = imageService.getImageById(imageId);
+            if (image != null) {
+                imageService.updateImage(file, imageId);
+                return ResponseEntity.ok(new ApiResponse("Update success!", null));
+            }
+
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Update failed!", null));
+    }
+
+    @DeleteMapping("/image/{imageId}/update")
+    public ResponseEntity<ApiResponse> deleteImage(@PathVariable Long imageId) {
+        try {
+            Image image = imageService.getImageById(imageId);
+            if (image != null) {
+                imageService.deleteImageById(imageId);
+                return ResponseEntity.ok(new ApiResponse("Delete success!", null));
+            }
+
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Delete failed!", null));
+    }
+
 }
+
+
+
+
+
+
+
+
+
--- src/main/java/com/qttp/demo_shops/service/category/ICategoryService.java
+++ src/main/java/com/qttp/demo_shops/service/category/ICategoryService.java
@@ -10,6 +10,6 @@ public interface ICategoryService {
     List<Category> getAllCategories();
     Category addCategory(Category category);
     Category updateCategory(Category category, Long id);
-    void deleteCategory(Long id);
+    void deleteCategoryById(Long id);
 
 }
